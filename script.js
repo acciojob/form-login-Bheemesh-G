60439afeaa786d7175eab86772777248171df2fb
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-09-18 22:46:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
 
+	const form = document.getElementById("form1");
+
+	form.addEventListener("submit",(event)=>{
+		event.preventDefault();
+		const formData = new FormData(form);
+		let firstName = formData.get("fname");
+		let lastName = formData.get("lname");
+		alert(`${firstName} ${lastName}`);
+	});
+
 }
